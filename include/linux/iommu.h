95a7fe9bd7fff978b5294e542edecc2aac1ca95d
Stepan Moskovchenko <stepanm@codeaurora.org>
2011-08-31 17:13:32 -0700

    drivers: iommu: Add flags to iommu_domain_alloc
    Add the ability to pass flags into the iommu_domain_alloc
    function to specify domain attributes.
    
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -107,7 +107,7 @@ enum iommu_attr {
  */
 struct iommu_ops {
 	bool (*capable)(enum iommu_cap);
-	int (*domain_init)(struct iommu_domain *domain);
+	int (*domain_init)(struct iommu_domain *domain, int flags);
 	void (*domain_destroy)(struct iommu_domain *domain);
 	int (*attach_dev)(struct iommu_domain *domain, struct device *dev);
 	void (*detach_dev)(struct iommu_domain *domain, struct device *dev);
@@ -150,7 +150,7 @@ struct iommu_ops {
 extern int bus_set_iommu(struct bus_type *bus, const struct iommu_ops *ops);
 extern bool iommu_present(struct bus_type *bus);
 extern bool iommu_capable(struct bus_type *bus, enum iommu_cap cap);
-extern struct iommu_domain *iommu_domain_alloc(struct bus_type *bus);
+extern struct iommu_domain *iommu_domain_alloc(struct bus_type *bus, int flags);
 extern struct iommu_group *iommu_group_get_by_id(int id);
 extern void iommu_domain_free(struct iommu_domain *domain);
 extern int iommu_attach_device(struct iommu_domain *domain,
@@ -265,7 +265,7 @@ static inline bool iommu_capable(struct bus_type *bus, enum iommu_cap cap)
 	return false;
 }
 
-static inline struct iommu_domain *iommu_domain_alloc(struct bus_type *bus)
+static inline struct iommu_domain *iommu_domain_alloc(struct bus_type *bus, int flags)
 {
 	return NULL;
 }
