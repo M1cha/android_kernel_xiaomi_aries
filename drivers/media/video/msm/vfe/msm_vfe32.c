ed89cb5e00c951b49a792b46a3bcc2be51c3dafd
Ohad Ben-Cohen <ohad@wizery.com>
2012-05-21 20:20:05 +0300

    iommu/core: pass a user-provided token to fault handlers
    Sometimes a single IOMMU user may have to deal with several
    different IOMMU devices (e.g. remoteproc).
    
    When an IOMMU fault happens, such users have to regain their
    context in order to deal with the fault.
    
    Users can't use the private fields of neither the iommu_domain nor
    the IOMMU device, because those are already used by the IOMMU core
    and low level driver (respectively).
    
    This patch just simply allows users to pass a private token (most
    notably their own context pointer) to iommu_set_fault_handler(),
    and then makes sure it is provided back to the users whenever
    an IOMMU fault happens.
    
    The patch also adopts remoteproc to the new fault handling
    interface, but the real functionality using this (recovery of
    remote processors) will only be added later in a subsequent patch
    set.
    
    Change-Id: Ic04659686e72838a0db518e9303dd037191e3879
    Cc: Fernando Guzman Lugo <fernando.lugo@ti.com>
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    [ohaugan@codeaurora.org: Resolved compilation and merge issues]
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp.c

--- drivers/media/video/msm/vfe/msm_vfe32.c
+++ drivers/media/video/msm/vfe/msm_vfe32.c
@@ -6262,7 +6262,7 @@ static const struct v4l2_subdev_ops msm_vfe_subdev_ops = {
 
 #if defined(CONFIG_MSM_IOMMU) && defined(VFE_IOMMU_FAULT_HANDLER)
 static int vfe_iommu_fault_handler(struct iommu_domain *domain,
-		struct device *dev, unsigned long iova, int flags)
+		struct device *dev, unsigned long iova, int flags, void* token)
 {
 	pr_err("iommu page fault has happened\n");
         atomic_set(&fault_recovery, 1);
@@ -6333,7 +6333,7 @@ int msm_axi_subdev_init(struct v4l2_subdev *sd,
 	}
 #ifdef VFE_IOMMU_FAULT_HANDLER
 	iommu_set_fault_handler(mctl->domain,
-                        vfe_iommu_fault_handler);
+                        vfe_iommu_fault_handler, sd);
 #endif
 
 #endif
