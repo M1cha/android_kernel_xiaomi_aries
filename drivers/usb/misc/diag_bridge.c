cf8a56a5f77cce9c066b3748c56c4e41eb80e83a
Hemant Kumar <hemantk@codeaurora.org>
2012-10-16 17:57:08 -0700

    usb: Decrease autosuspend delay to 200ms upon usb disconnect
    Reduce overall HSIC wakelock active time by decreasing autosuspend
    delay for HSIC mdm device from 1 sec to 200ms. However when diag is
    active it would send poll events for every 500ms causing frequent
    supsend/resume. Avoid this by increasing autosuspend delay to 1 sec
    when diag is active. Also, set autosuspend delay for root hub to 0 by
    default. These changes reduce time HSIC controller is active when the
    bus is idle for better power savings.
    
    CRs-Fixed: 404378
    Change-Id: I0500f6b125dff158e6e0c10f3f2023b18ce9dd30
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/misc/diag_bridge.c
+++ drivers/usb/misc/diag_bridge.c
@@ -45,6 +45,7 @@ struct diag_bridge {
 	struct diag_bridge_ops	*ops;
 	struct platform_device	*pdev;
 	int			id;
+	unsigned		default_autosusp_delay;
 
 	/* debugging counters */
 	unsigned long		bytes_to_host;
@@ -76,6 +77,12 @@ int diag_bridge_open(int id, struct diag_bridge_ops *ops)
 
 	dev->ops = ops;
 	dev->err = 0;
+
+#ifdef CONFIG_PM_RUNTIME
+	dev->default_autosusp_delay = dev->udev->dev.power.autosuspend_delay;
+#endif
+	pm_runtime_set_autosuspend_delay(&dev->udev->dev,
+			AUTOSUSP_DELAY_WITH_USB);
 
 	kref_get(&dev->kref);
 
@@ -117,6 +124,10 @@ void diag_bridge_close(int id)
 
 	usb_kill_anchored_urbs(&dev->submitted);
 	dev->ops = 0;
+
+	pm_runtime_set_autosuspend_delay(&dev->udev->dev,
+			dev->default_autosusp_delay);
+
 	kref_put(&dev->kref, diag_bridge_delete);
 }
 EXPORT_SYMBOL(diag_bridge_close);
