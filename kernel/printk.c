fb246c2e7498c3fa7199491c8b4f10e3fc4724a8
Arve Hjønnevåg <arve@android.com>
2013-06-14 19:54:40 -0700

    ARM: Fix "Make low-level printk work" to use a separate config option
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -51,7 +51,7 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
-#ifdef        CONFIG_DEBUG_LL
+#ifdef CONFIG_EARLY_PRINTK_DIRECT
 extern void printascii(char *);
 #endif
 
@@ -1826,10 +1826,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 	 */
 	text_len = vscnprintf(text, sizeof(textbuf), fmt, args);
 
-#ifdef	CONFIG_DEBUG_LL
-	printascii(text);
-#endif
-
 	/* mark and strip a trailing newline */
 	if (text_len && text[text_len-1] == '\n') {
 		text_len--;
@@ -1855,6 +1851,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 			text = (char *)end_of_header;
 		}
 	}
+
+#ifdef CONFIG_EARLY_PRINTK_DIRECT
+	printascii(text);
+#endif
 
 	if (level == -1)
 		level = default_message_loglevel;
