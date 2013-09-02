9763f8524b039c79b7972ef5ed03f8503bf734a2
M1cha <sigmaepsilon92@gmail.com>
2013-09-02 15:53:08 +0200

    msm: lge: mako: switch from TPA2028D to ES310
    Conflicts:
    	arch/arm/mach-msm/lge/mako/board-mako-sound.c

--- arch/arm/mach-msm/lge/mako/board-mako-sound.c
+++ arch/arm/mach-msm/lge/mako/board-mako-sound.c
@@ -21,20 +21,14 @@
 #include <mach/board_lge.h>
 #include "devices.h"
 
-#ifdef CONFIG_SND_SOC_TPA2028D
-#include <sound/tpa2028d.h>
+#ifdef CONFIG_SND_SOC_ES310
+#include <sound/es310.h>
 #endif
 
 #include "board-mako.h"
 #include "board-mako-earjack-debugger.h"
 
-
-#define TPA2028D_ADDRESS (0xB0>>1)
-#define MSM_AMP_EN (PM8921_GPIO_PM_TO_SYS(19))
-#define AGC_COMPRESIION_RATE        0
-#define AGC_OUTPUT_LIMITER_DISABLE  1
-#define AGC_FIXED_GAIN              12
-
+#define ES310_ADDRESS (0x3E)
 
 #define GPIO_EAR_MIC_BIAS_EN        PM8921_GPIO_PM_TO_SYS(20)
 #define GPIO_EAR_SENSE_N            82
@@ -43,70 +37,55 @@
 #define GPIO_EARPOL_DETECT          PM8921_GPIO_PM_TO_SYS(32)
 #define GPIO_EAR_KEY_INT            83
 
-#ifdef CONFIG_SND_SOC_TPA2028D
-int amp_enable(int on_state)
+#ifdef CONFIG_SND_SOC_ES310
+static int es310_power_setup(int on)
 {
-	int err = 0;
-	static int init_status = 0;
-	struct pm_gpio param = {
-		.direction      = PM_GPIO_DIR_OUT,
-		.output_buffer  = PM_GPIO_OUT_BUF_CMOS,
-		.output_value   = 1,
-		.pull           = PM_GPIO_PULL_NO,
-		.vin_sel	= PM_GPIO_VIN_S4,
-		.out_strength   = PM_GPIO_STRENGTH_MED,
-		.function       = PM_GPIO_FUNC_NORMAL,
-	};
+	int rc;
+	struct regulator *es310_vdd;
 
-	if (init_status == 0) {
-		err = gpio_request(MSM_AMP_EN, "AMP_EN");
-		if (err)
-			pr_err("%s: Error requesting GPIO %d\n",
-					__func__, MSM_AMP_EN);
+	es310_vdd = regulator_get(NULL, "8921_l15");
 
-		err = pm8xxx_gpio_config(MSM_AMP_EN, &param);
-		if (err)
-			pr_err("%s: Failed to configure gpio %d\n",
-					__func__, MSM_AMP_EN);
-		else
-			init_status++;
+	if (IS_ERR(es310_vdd)) {
+		printk(KERN_ERR "%s: Unable to get regulator es310_vdd\n",
+		       __func__);
+		return -1;
 	}
 
-	switch (on_state) {
-	case 0:
-		err = gpio_direction_output(MSM_AMP_EN, 0);
-		printk(KERN_INFO "%s: AMP_EN is set to 0\n", __func__);
-		break;
-	case 1:
-		err = gpio_direction_output(MSM_AMP_EN, 1);
-		printk(KERN_INFO "%s: AMP_EN is set to 1\n", __func__);
-		break;
-	case 2:
-		printk(KERN_INFO "%s: amp enable bypass(%d)\n", __func__, on_state);
-		err = 0;
-		break;
-
-	default:
-		pr_err("amp enable fail\n");
-		err = 1;
-		break;
+	rc = regulator_set_voltage(es310_vdd, 2800000, 2800000);
+	if (rc) {
+		printk(KERN_ERR "%s: regulator set voltage failed\n", __func__);
+		return rc;
 	}
-	return err;
+
+	rc = regulator_enable(es310_vdd);
+	if (rc) {
+		printk(KERN_ERR
+		       "%s: Error while enabling regulator for es310 %d\n",
+		       __func__, rc);
+		return rc;
+	}
+
+	return 0;
 }
 
-static struct audio_amp_platform_data amp_platform_data =  {
-	.enable = amp_enable,
-	.agc_compression_rate = AGC_COMPRESIION_RATE,
-	.agc_output_limiter_disable = AGC_OUTPUT_LIMITER_DISABLE,
-	.agc_fixed_gain = AGC_FIXED_GAIN,
+#define MITWO_GPIO_ES310_CLK		34
+#define MITWO_GPIO_ES310_RESET		37
+#define MITWO_GPIO_ES310_MIC_SWITCH	38
+#define MITWO_GPIO_ES310_WAKEUP		51
+static struct es310_platform_data audience_es310_pdata = {
+	.gpio_es310_reset = MITWO_GPIO_ES310_RESET,
+	.gpio_es310_clk = MITWO_GPIO_ES310_CLK,
+	.gpio_es310_wakeup = MITWO_GPIO_ES310_WAKEUP,
+	.gpio_es310_mic_switch = MITWO_GPIO_ES310_MIC_SWITCH,
+	.power_on = es310_power_setup,
 };
 #endif
 
 static struct i2c_board_info msm_i2c_audiosubsystem_info[] = {
-#ifdef CONFIG_SND_SOC_TPA2028D
+#ifdef CONFIG_SND_SOC_ES310
 	{
-		I2C_BOARD_INFO("tpa2028d_amp", TPA2028D_ADDRESS),
-		.platform_data = &amp_platform_data,
+		I2C_BOARD_INFO("audience_es310", ES310_ADDRESS),
+		.platform_data = &audience_es310_pdata,
 	}
 #endif
 };
@@ -119,7 +98,7 @@ static struct i2c_registry msm_i2c_audiosubsystem __initdata = {
 	ARRAY_SIZE(msm_i2c_audiosubsystem_info),
 };
 
-static void __init lge_add_i2c_tpa2028d_devices(void)
+static void __init lge_add_i2c_es310_devices(void)
 {
 	/* Run the array and install devices as appropriate */
 	i2c_register_board_info(msm_i2c_audiosubsystem.bus,
@@ -273,6 +252,6 @@ void __init lge_add_sound_devices(void)
 #ifdef CONFIG_SWITCH_FSA8008
 	mako_fixed_audio();
 #endif
-	lge_add_i2c_tpa2028d_devices();
+	lge_add_i2c_es310_devices();
 	platform_add_devices(sound_devices, ARRAY_SIZE(sound_devices));
 }
