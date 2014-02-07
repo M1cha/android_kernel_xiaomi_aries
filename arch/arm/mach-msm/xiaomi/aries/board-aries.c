69467098fcbb268bd525af884922c223662718e2
M1cha <sigmaepsilon92@gmail.com>
2014-02-07 13:04:35 +0100

    aries: legacy camera support

--- arch/arm/mach-msm/xiaomi/aries/board-aries.c
+++ arch/arm/mach-msm/xiaomi/aries/board-aries.c
@@ -2238,6 +2238,13 @@ static void __init register_i2c_devices(void)
 		apq8064_camera_board_info.board_info,
 		apq8064_camera_board_info.num_i2c_board_info,
 	};
+	/* Enabling flash LED for camera */
+	struct i2c_registry apq8064_xiaomi_camera_i2c_devices = {
+		I2C_FFA,
+		APQ_8064_GSBI1_QUP_I2C_BUS_ID,
+		apq8064_xiaomi_camera_board_info.board_info,
+		apq8064_xiaomi_camera_board_info.num_i2c_board_info,
+	};
 #endif
 
 
@@ -2245,6 +2252,11 @@ static void __init register_i2c_devices(void)
 	i2c_register_board_info(apq8064_camera_i2c_devices.bus,
 		apq8064_camera_i2c_devices.info,
 		apq8064_camera_i2c_devices.len);
+
+	/* Enabling flash LED for camera */
+	i2c_register_board_info(apq8064_xiaomi_camera_i2c_devices.bus,
+		apq8064_xiaomi_camera_i2c_devices.info,
+		apq8064_xiaomi_camera_i2c_devices.len);
 #endif
 }
 
