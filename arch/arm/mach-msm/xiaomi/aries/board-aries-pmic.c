a63a7b96be85ee8377d65e0b4642b65e651a23cb
M1cha <sigmaepsilon92@gmail.com>
2014-07-06 15:57:26 +0200

    aries: pmic: fix notification led's
    Change-Id: I5a3eb246bbf3b256a0607dc3f98235c693701f2f

--- arch/arm/mach-msm/xiaomi/aries/board-aries-pmic.c
+++ arch/arm/mach-msm/xiaomi/aries/board-aries-pmic.c
@@ -214,13 +214,13 @@ static struct pm8xxx_misc_platform_data apq8064_pm8921_misc_pdata = {
 
 static struct led_info pm8921_led_info[] = {
 	[0] = {
-		.name			= "led:red",
+		.name			= "red",
 	},
 	[1] = {
-		.name			= "led:green",
+		.name			= "green",
 	},
 	[2] = {
-		.name			= "led:blue",
+		.name			= "blue",
 	},
 };
 
