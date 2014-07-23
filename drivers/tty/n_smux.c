9b13d29feb74191b013d871d7cd24106940e7fa7
M1cha <sigmaepsilon92@gmail.com>
2014-07-23 15:12:32 +0200

    tty: n_smux: fix merge fails

--- drivers/tty/n_smux.c
+++ drivers/tty/n_smux.c
@@ -1744,7 +1744,7 @@ static int smux_handle_rx_status_cmd(struct smux_pkt_t *pkt)
  */
 static int smux_handle_rx_power_cmd(struct smux_pkt_t *pkt)
 {
-	struct smux_pkt_t *ack_pkt;
+	struct smux_pkt_t *ack_pkt = NULL;
 	int power_down = 0;
 	unsigned long flags;
 
